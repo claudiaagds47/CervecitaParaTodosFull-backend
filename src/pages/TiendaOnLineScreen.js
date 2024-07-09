74e8d87383ac9c6a5beb73c6ed9b3e05f4a9c316
Alexis Llano <alexisg.llano@gmail.com>
2024-07-09 18:45:12 -0300

    agregue page tienda

--- src/pages/TiendaOnLineScreen.js
+++ src/pages/TiendaOnLineScreen.js
@@ -4,9 +4,12 @@ import React from 'react'
 const TiendaOnLine = () => {
   return (
     <>
-     
+      <Banner />
+      <Navigation />
+      <div className="containerTarjetas"></div>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default TiendaOnLine
