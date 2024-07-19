c43007d99200dd10ea3bbbca51c5252b3b29d9cd
Alexis Llano <alexisg.llano@gmail.com>
2024-07-19 00:09:37 -0300

    refactor css.v2

--- src/_app.js
+++ src/_app.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import '@/styles/globals.js';
-import '@/styles/Default.js';
-
-
+import React from "react";
+import "@/styles/globals.js";
+import "@/styles/Default.js";
+import GlobalStyle from "./styles/GlobalStyle";
 
 // import '@/styles/style.css';
 
-
-
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />;
+    </>
+  );
 }
